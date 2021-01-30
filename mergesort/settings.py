c710be352c031b5d88508869338c42286fb123ed
Jonathan Ting <jting@vols.utk.edu>
2021-01-30 17:19:49 -0600

    in-progress

--- mergesort/settings.py
+++ mergesort/settings.py
@@ -32,12 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'sorter.apps.SorterConfig',
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
---
+++ spotify/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from .credentials import REDIRECT_URI, CLIENT_SECRET, CLIENT_ID
+from rest_framework.views import APIView
+
