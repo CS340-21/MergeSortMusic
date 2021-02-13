dc3343f9375b9d199745f7d58eaf4a6808e99c0d
Jonathan Ting <jting@vols.utk.edu>
2021-02-13 00:45:25 -0600

    up to tut12 done

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
 from .models import Room
 
+
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        # Instead of mentioning all the fields (one by one) in Meta class, you can simply put fields = '__all__'
-        fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
+        fields = ('id', 'code', 'host', 'guest_can_pause',
+                  'votes_to_skip', 'created_at')
 
 
 class CreateRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('guest_can_pause', 'votes_to_skip')
+
+
+class UpdateRoomSerializer(serializers.ModelSerializer):
+    code = serializers.CharField(validators=[])
+
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip', 'code')
