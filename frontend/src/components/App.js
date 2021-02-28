8224762194d03a7a16cdf52f219027d043e72194
Jonathan Ting <jting@vols.utk.edu>
2021-02-28 16:28:41 -0600

    Converted App.js to functional

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import RoomJoinPage from "./RoomJoinPage";
@@ -7,26 +7,20 @@ import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
 import HomePage from "./HomePage";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <>
-        <Router>
-          <Switch>
-            <Route path="/join" component={RoomJoinPage} />
-            <Route path="/info" component={Info} />
-            <Route path="/create" component={CreateRoomPage} />
-            <Route path="/room/:roomCode" component={Room} />
-            <Route path="/" component={HomePage} />
-          </Switch>
-        </Router>
-      </>
-    );
-  }
+export default function App(props) {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/info" component={Info} />
+          <Route path="/create" component={CreateRoomPage} />
+          <Route path="/room/:roomCode" component={Room} />
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 const appDiv = document.getElementById("app");
