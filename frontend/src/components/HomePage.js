9a8bc0dcff447cdc76e05918c499ebf4b1ab603a
Jonathan Ting <jting@vols.utk.edu>
2021-02-28 17:18:24 -0600

    Converted HomePage.js to functional

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,48 +1,41 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
-export default class HomePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      roomCode: null,
-    };
-  }
+export default function HomePage(props) {
+  const [roomCode,setRoomCode] = useState(null);
 
-  async componentDidMount() {
+  useEffect(async () => {
     fetch("/api/user-in-room")
       .then(response => response.json())
       .then(data => {
-        this.setState({
-          roomCode: data.code,
-        });
-        if (this.state.roomCode !== null) {this.props.history.push(`/room/${this.state.roomCode}`);}
+        setRoomCode(data.code);
+        if (roomCode !== null) {
+          props.history.push(`/room/${roomCode}`);
+        }
       });
-  }
+  });
 
-  render() {
-    return (
-      <Grid container spacing={3} align="center">
-        <Grid item xs={12}>
-          <Typography variant="h3" compact="h3">
-            House Party
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <ButtonGroup disableElevation variant="contained" color="primary">
-            <Button color="primary" to="/join" component={Link}>
-              Join a Room
-            </Button>
-            <Button color="default" to="/info" component={Link}>
-              Info
-            </Button>
-            <Button color="secondary" to="/create" component={Link}>
-              Create a Room
-            </Button>
-          </ButtonGroup>
-        </Grid>
+  return (
+    <Grid container spacing={3} align="center">
+      <Grid item xs={12}>
+        <Typography variant="h3" compact="h3">
+          House Party
+        </Typography>
       </Grid>
-    );
-  }
+      <Grid item xs={12}>
+        <ButtonGroup disableElevation variant="contained" color="primary">
+          <Button color="primary" to="/join" component={Link}>
+            Join a Room
+          </Button>
+          <Button color="default" to="/info" component={Link}>
+            Info
+          </Button>
+          <Button color="secondary" to="/create" component={Link}>
+            Create a Room
+          </Button>
+        </ButtonGroup>
+      </Grid>
+    </Grid>
+  );
 }
