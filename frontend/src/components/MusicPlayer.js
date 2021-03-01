3f53eea649f2ef1840d00041d2c7eed62d4887ae
Jonathan Ting <jting@vols.utk.edu>
2021-02-28 18:02:26 -0600

    Converted MusicPlayer.js to

--- frontend/src/components/MusicPlayer.js
+++ frontend/src/components/MusicPlayer.js
@@ -1,13 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import { Grid, Typography, Card, IconButton, LinearProgress } from "@material-ui/core";
 import { PlayArrow, Pause, SkipNext } from "@material-ui/icons";
 
-export default class MusicPlayer extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  skipSong() {
+export default function MusicPlayer(props) {
+  const skipSong = () => {
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -15,7 +11,7 @@ export default class MusicPlayer extends Component {
     fetch("/spotify/skip", requestOptions);
   }
 
-  pauseSong() {
+  const pauseSong = () => {
     const requestOptions = {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
@@ -23,7 +19,7 @@ export default class MusicPlayer extends Component {
     fetch("/spotify/pause", requestOptions);
   }
 
-  playSong() {
+  const playSong = () => {
     const requestOptions = {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
@@ -31,39 +27,37 @@ export default class MusicPlayer extends Component {
     fetch("/spotify/play", requestOptions);
   }
 
-  render() {
-    const songProgress = (this.props.time / this.props.duration) * 100;
+  const songProgress = (props.time / props.duration) * 100;
 
-    return (
-      <Card>
-        <Grid container alignItems="center" align="center">
-          <Grid item xs={4}>
-            <img src={this.props.image_url} height="100%" width="100%" />
-          </Grid>
-          <Grid item xs={8}>
-            <Typography component="h5" variant="h5">
-              {this.props.title}
-            </Typography>
-            <Typography color="textSecondary" variant="subtitle1">
-              {this.props.artist}
-            </Typography>
-            <div>
-              <IconButton
-                onClick={() => {
-                  this.props.is_playing ? this.pauseSong() : this.playSong();
-                }}
-              >
-                {this.props.is_playing ? <Pause /> : <PlayArrow />}
-              </IconButton>
-              <IconButton onClick={() => this.skipSong()}>
-                {this.props.votes} / {this.props.votes_required}
-                <SkipNext />
-              </IconButton>
-            </div>
-          </Grid>
+  return (
+    <Card>
+      <Grid container alignItems="center" align="center">
+        <Grid item xs={4}>
+          <img src={props.image_url} height="100%" width="100%" />
         </Grid>
-        <LinearProgress variant="determinate" value={songProgress} />
-      </Card>
-    );
-  }
+        <Grid item xs={8}>
+          <Typography component="h5" variant="h5">
+            {props.title}
+          </Typography>
+          <Typography color="textSecondary" variant="subtitle1">
+            {props.artist}
+          </Typography>
+          <div>
+            <IconButton
+              onClick={() => {
+                props.is_playing ? pauseSong() : playSong();
+              }}
+            >
+              {props.is_playing ? <Pause /> : <PlayArrow />}
+            </IconButton>
+            <IconButton onClick={() => skipSong()}>
+              {props.votes} / {props.votes_required}
+              <SkipNext />
+            </IconButton>
+          </div>
+        </Grid>
+      </Grid>
+      <LinearProgress variant="determinate" value={songProgress} />
+    </Card>
+  );
 }
