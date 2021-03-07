906a3056f3f146c53cb999d58a6fa5f05fa477a8
jonting <jting@vols.utk.edu>
2021-03-07 00:46:48 -0600

    Added button for Spotify authentication

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import clsx from "clsx";
 
 import { makeStyles } from "@material-ui/core/styles";
 import {
   AppBar,
   Toolbar,
   IconButton,
+  Button,
   Drawer,
   List,
   Divider,
   ListItem,
   ListItemIcon,
   ListItemText,
+  Typography,
 } from "@material-ui/core";
 import { Home, Menu, Inbox, Mail } from "@material-ui/icons";
 
@@ -30,17 +33,36 @@ const useStyles = makeStyles((theme) => ({
     padding: "0px",
     margin: "0px",
   },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textDecoration: "none",
+  },
   list: {
     width: 250,
   },
   fullList: {
     width: "auto",
   },
+  spotifyButton: {
+    backgroundColor: "#1DB954",
+    opacity: "1",
+  },
+  spotifyDisabled: {
+    backgroundColor: "#1DB954",
+    opacity: "0.4",
+  },
 }));
 
 export default function App(props) {
   const classes = useStyles();
-  const [state, setState] = React.useState({ left: false });
+  const [state, setState] = useState({ left: false });
+  const [spotifyAuthenticated, setSpotifyAuthenticated] = useState(true);
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (
@@ -51,6 +73,31 @@ export default function App(props) {
     }
 
     setState({ [anchor]: open });
+  };
+
+  const authenticateSpotify = () => {
+    fetch("/spotify/is-authenticated")
+      .then((response) => response.json())
+      .then((data) => {
+        setSpotifyAuthenticated(data.status);
+        console.log(data.status);
+        if (!data.status) {
+          fetch("/spotify/get-auth-url")
+            .then((response) => response.json())
+            .then((data) => {
+              window.location.replace(data.url);
+            });
+        }
+      });
+  };
+
+  const checkAuthentication = () => {
+    fetch("/spotify/is-authenticated")
+      .then((response) => response.json())
+      .then((data) => {
+        setSpotifyAuthenticated(data.status);
+        console.log(data.status);
+      });
   };
 
   const list = (anchor) => (
@@ -80,6 +127,16 @@ export default function App(props) {
     </div>
   );
 
+  useEffect(() => {
+    console.log("ran");
+    checkAuthentication();
+    const interval = setInterval(() => {checkAuthentication()}, 3000);
+    return () => {
+      console.log("cleanup");
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className={classes.gradient}>
       <Router>
@@ -88,19 +145,33 @@ export default function App(props) {
             <IconButton
               onClick={toggleDrawer("left", true)}
               edge="start"
+              className={classes.menuButton}
+              color="inherit"
               aria-label="menu"
             >
               <Menu />
             </IconButton>
-            <IconButton
-              edge="start"
+            <Typography
               color="inherit"
+              variant="h6"
               to="/"
               component={Link}
               aria-label="home"
+              className={classes.title}
             >
-              <Home fontSize="large" />
-            </IconButton>
+              Merge Sort Music
+            </Typography>
+            <Button
+              color="inherit"
+              className={classes.spotifyButton}
+              disabled={spotifyAuthenticated}
+              variant="contained"
+              onClick={() => {
+                authenticateSpotify();
+              }}
+            >
+              {spotifyAuthenticated ? "Authenticated" : "Authenticate with Spotify"}
+            </Button>
           </Toolbar>
         </AppBar>
 
