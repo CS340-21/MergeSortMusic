585c0272e3cfe6f7b3d3b9ba4415e020e3e26589
Shreyank Patel <spatel91@vols.utk.edu>
2021-04-22 23:17:02 -0400

    embed song player

--- frontend/src/components/SongCard.js
+++ frontend/src/components/SongCard.js
@@ -4,6 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
@@ -23,6 +24,7 @@ export default function SongCard(props) {
       "songName"  : props.song['track_name'],
       "artistName": props.song['artist'],
       "albumArt"  : props.song['image_300'],
+      "track_uri" : "https://open.spotify.com/embed/track/".concat(props.song['track_uri'])
   }
 
   return (
@@ -44,7 +46,10 @@ export default function SongCard(props) {
             {song.songName}
           </Typography>
         </CardContent>
-      </CardActionArea>=
+      </CardActionArea>
+      <CardActions>
+        <iframe src={song.track_uri} display="none" width="300" height="75" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      </CardActions>
     </Card>
   );
 }
